832bcfc26f05c0bc979c2ba6b3893c9ffa2009f1
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-14 19:52:37 +0100

    rafactor: move data layer interfaces to folders

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,5 +1,5 @@
 import { AuthenticationModel, Authentication } from '../../../domain/usecases/authentication'
-import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
