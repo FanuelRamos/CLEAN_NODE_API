a01a8a9af50daa6e78b704ed5f4df6550454b05d
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-17 08:51:45 +0100

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://127.0.0.1:27017/clean-node-api',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'tj670==5H'
 }
