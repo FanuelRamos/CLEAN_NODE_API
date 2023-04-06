bd44b02b67ff425f334ada8ec64a87cfd7c9b85c
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-06 22:20:50 +0100

    feat: ensure SigUpController returns an accessToken on seccess

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -16,18 +16,18 @@ export class SignUpController implements Controller {
       }
       const { name, email, password } = httpRequest.body
 
-      const account = await this.addAccount.add({
+      await this.addAccount.add({
         name,
         email,
         password
       })
 
-      await this.authentication.auth({
+      const accessToken = await this.authentication.auth({
         email,
         password
       })
 
-      return ok(account)
+      return ok({ accessToken })
     } catch (error) {
       console.error(error)
       return serverError(error)
