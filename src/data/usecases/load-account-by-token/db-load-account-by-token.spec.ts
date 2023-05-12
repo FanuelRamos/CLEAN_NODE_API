13fa2008cf1b6db71d91ef889dd8da900ac21496
Fanuel Ramos <fanuelramos111@gmail.com>
2023-05-12 15:20:06 +0100

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -68,4 +68,13 @@ describe('Should call Decrypter with correct values', () => {
     await sut.load('any_token', 'any_role')
     expect(loadByTokenSpy).toHaveBeenLastCalledWith('any_token', 'any_role')
   })
+
+  test('Should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByTokenRepositoryStub, 'loadByToken').mockImplementationOnce((): any => {
+      return Promise.resolve(null)
+    })
+    const account = await sut.load('any_token', 'any_role')
+    expect(account).toBeNull()
+  })
 })
